fd6cf3e772588c38234f49323a36bcc8fbf5e8a8
Bruno Campos <brunocampos@gmail.com>
2018-11-07 19:41:42 -0200

    calculate refactor

--- calculator.js
+++ calculator.js
@@ -1,11 +1,10 @@
 function calculate(string) {
-  var numbers = [];
-  var hasOperator = false;
-
-  if (!string || string.length < 1 || isNaN(string[0])) {
+  if (isInvalidExpression(string)) {
     return 0;
   }
 
+  let numbers = [];
+  let hasOperator = false;
   let expressionArr = string.split(' ');
 
   for (let i = 0; i < expressionArr.length; i++) {
@@ -13,24 +12,9 @@ function calculate(string) {
       numbers.push(parseFloat(expressionArr[i]));
     } else {
       hasOperator = true;
-
       let last2Numbers = numbers.splice(-2);
 
-      if (expressionArr[i] == '+') {
-        numbers.push(last2Numbers[0] + last2Numbers[1]);
-      } else if (expressionArr[i] == '-') {
-        numbers.push(last2Numbers[0] - last2Numbers[1]);
-      } else if (expressionArr[i] == '/') {
-        if(last2Numbers[1] == 0) {
-          numbers.push(0);
-        } else {
-          numbers.push(last2Numbers[0] / last2Numbers[1]);
-        }
-      } else if (expressionArr[i] == '*') {
-        numbers.push(last2Numbers[0] * last2Numbers[1]);
-      } else if(expressionArr[i] == '^') {
-        numbers.push(last2Numbers[0] ** last2Numbers[1]);
-      }
+      numbers.push(operate(...last2Numbers, expressionArr[i]));
     }
   }
 
@@ -40,3 +24,30 @@ function calculate(string) {
     return numbers[numbers.length - 1];
   }
 }
+
+function isInvalidExpression(string) {
+  return !string || string.length < 1 || isNaN(string[0]);
+}
+
+function operate(value1, value2, operator) {
+  switch(operator) {
+    case '+':
+      return value1 + value2;
+    case '-':
+      return value1 - value2;
+    case '/':
+      return (value2 == 0) ? 0 : value1 / value2;
+    case '*':
+      return value1 * value2;
+    case '^':
+      return value1 ** value2;
+  }
+}
+
+// Improvements:
+// * An empty expression should evaluate to 0: bug fixed with ''
+// * Return NaN for divisions by 0
+// * Make test suite one expectation per test
+// * Improve readme with test instructions
+
+
